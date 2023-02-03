af60b6fe55a9c2763466e239813057b77948793e
munnarae <skfo789@naver.com>
2023-02-03 19:21:35 +0900

    [Feature] 아코디언 기능 구현 완료#40

--- client/js/productList.js
+++ client/js/productList.js
@@ -1,12 +1,12 @@
-// $(function () {
-//   //header menu------------------------------------
-//   $('.accordion--set__head').click(function () {
-//     $(this)
-//       .find('.accordion--set__body')
-//       .stop()
-//       .slideDown();
-//   });
-//   $('.accordion--set__head').click(function () {
-//     $('.accordion--set__body').stop().slideUp();
-//   });
-// });
+$('.accordion--set__head').click(function () {
+  if (
+    $(this).find('.accordion--set__body').is(':visible')
+  ) {
+    $(this).find('.accordion--set__body').stop().slideUp();
+  } else {
+    $(this)
+      .find('.accordion--set__body')
+      .stop()
+      .slideDown();
+  }
+});
