e429b877bbc2c6f9922a161b2925f13447a426a5
leejungho <ljhmoa@gmail.com>
2023-02-04 20:57:47 +0900

    [Add] today-recomend-product 기본 swiper 구현 #46

--- client/js/swiper.js
+++ client/js/swiper.js
@@ -7,23 +7,45 @@ let mainBannerSwiper = new Swiper('.swiper1', {
     disableOnInteraction: false,
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper1 .swiper-pagination',
     type: 'fraction',
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper1 .swiper-button-next',
+    prevEl: '.swiper1 .swiper-button-prev',
   },
 });
 
 // mousehover stop start 설정
-$('.swiper-slide').on('mouseenter', function () {
+$('.swiper1 .swiper-slide').on('mouseenter', function () {
   mainBannerSwiper.autoplay.stop();
-  $('.swiper-button-next').addClass('buttonhidden');
-  $('.swiper-button-prev').addClass('buttonhidden');
+  $('.swiper1 .swiper-button-next').addClass(
+    'buttonhidden'
+  );
+  $('.swiper1 .swiper-button-prev').addClass(
+    'buttonhidden'
+  );
 });
-$('.swiper-slide').on('mouseleave', function () {
+$('.swiper1 .swiper-slide').on('mouseleave', function () {
   mainBannerSwiper.autoplay.start();
-  $('.swiper-button-next').removeClass('buttonhidden');
-  $('.swiper-button-prev').removeClass('buttonhidden');
+  $('.swiper1 .swiper-button-next').removeClass(
+    'buttonhidden'
+  );
+  $('.swiper1 .swiper-button-prev').removeClass(
+    'buttonhidden'
+  );
+});
+
+// today-recomend-product swiper2
+let todayRecomendProductSwiper = new Swiper('.swiper2', {
+  slidesPerView: '4', // 한 슬라이드에 보여줄 갯수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  pagination: {
+    el: '.swiper2 .swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper2 .swiper-button-next',
+    prevEl: '.swiper2 .swiper-button-prev',
+  },
 });
