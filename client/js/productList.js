36c7ed2ce9c59b49487614b6ed2dc93cff9676e1
munnarae <skfo789@naver.com>
2023-02-05 18:06:41 +0900

    [Feature] 초기화버튼 구현완료#41

--- client/js/productList.js
+++ client/js/productList.js
@@ -1,12 +1,27 @@
-$('.accordion--set__head').click(function () {
-  if (
-    $(this).find('.accordion--set__body').is(':visible')
-  ) {
-    $(this).find('.accordion--set__body').stop().slideUp();
-  } else {
-    $(this)
-      .find('.accordion--set__body')
-      .stop()
-      .slideDown();
-  }
+// 슬라이드 기능
+
+$(document).ready(function () {
+  $('.accordion--set__head>button').click(function () {
+    var submenu = $(this).next('ul');
+
+    if (submenu.is(':visible')) {
+      submenu.slideUp();
+    } else {
+      submenu.slideDown();
+    }
+  });
 });
+
+// 체크 초기화
+const initializationButton = document.querySelector(
+  '.initialization'
+);
+const checkbutton = document.querySelectorAll('.check2');
+
+let han = () => {
+  checkbutton.forEach((item) => {
+    item.checked = false;
+  });
+};
+
+initializationButton.addEventListener('click', han);
