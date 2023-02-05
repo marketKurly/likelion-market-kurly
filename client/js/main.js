c76333e35d3edfe395bc9937437482a72c67aa68
munnarae <skfo789@naver.com>
2023-02-05 22:25:09 +0900

    [Feature] 헤더 푸터 수정 삽입#41

---
+++ client/js/main.js
@@ -0,0 +1,43 @@
+import { getNode, attr } from '../lib/index.js';
+
+const header = getNode('.header');
+const navHeaderButton = getNode('.nav__header__button');
+const navCategoryWrapper = getNode(
+  '.nav__category__wrapper'
+);
+
+document.documentElement.addEventListener(
+  'mouseover',
+  visibleCategory
+);
+navCategoryWrapper?.addEventListener(
+  'mouseout',
+  invisibleCategory
+);
+navHeaderButton?.addEventListener(
+  'mouseout',
+  invisibleCategory
+);
+
+function visibleCategory(e) {
+  e.stopPropagation();
+  if (
+    attr(e.target, 'class') === 'nav__header__button' ||
+    attr(e.target, 'class')?.includes('nav__category')
+  ) {
+    navCategoryWrapper.style.display = 'block';
+  }
+}
+
+function invisibleCategory(e) {
+  if (attr(e.target, 'class')?.includes('nav__category'))
+    navCategoryWrapper.style.display = 'none';
+}
+
+// 탑 ad광고 창 닫기
+let button = document.querySelector('.topbanner__close');
+function handler() {
+  let AD = document.querySelector('.topbanner');
+  AD.style.display = 'none';
+}
+button.addEventListener('click', handler);
