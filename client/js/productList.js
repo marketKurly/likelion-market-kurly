189a640fdddbc4478be82e340c8aac02802bac2e
munnarae <skfo789@naver.com>
2023-02-08 04:08:23 +0900

    [Fix] 상품이미지 데이터 불러오기 완성#70

--- client/js/productList.js
+++ client/js/productList.js
@@ -1,5 +1,12 @@
-// 슬라이드 기능
+import {
+  getNode,
+  attr,
+  insertLast,
+  tiger,
+  xhrPromise,
+} from '../lib/index.js';
 
+// 슬라이드 기능
 $(document).ready(function () {
   $('.accordion--set__head>button').click(function () {
     var submenu = $(this).next('ul');
@@ -26,44 +33,105 @@ resetButton.addEventListener('click', a);
 
 // 페이지네이션
 
-$(function () {
-  let container = $('#pagination');
-  container.pagination({
-    dataSource: [
-      {},
-      {},
-      {},
-      {},
-      {},
-      {},
-      {},
-      {},
-      {},
-      {},
-      {},
-      {},
-      {},
-      {},
-      {},
-    ],
-    pageSize: 5,
-    callback: function (data, pagination) {
-      var dataHtml;
-
-      $.each(data, function (index, item) {
-        dataHtml + item.name;
-      });
-
-      $('#data-container').html(dataHtml);
-    },
-  });
-});
-
 // 상품 주소 받아오기
-
-fetch('http://localhost:3000/products')
-  .then((res) => res.json())
+const getItemList = getNode('.product__list--grid');
+tiger.get('http://localhost:3000/products');
+xhrPromise
+  .get('http://localhost:3000/products')
   .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => console.error(error));
+    $(function () {
+      let container = $('#pagination');
+      container.pagination({
+        dataSource: data,
+        pageSize: 5,
+        callback: function (data, pagination) {
+          var dataHtml = '<ul>';
+
+          $.each(data, function (index, item) {
+            dataHtml += '<li>' + item.name + '</li>';
+          });
+
+          dataHtml += '</ul>';
+
+          $('#data-container').html(dataHtml);
+        },
+      });
+    });
+
+    data.forEach((item, index) => {
+      let id = item.id;
+      let name = item.name;
+      let saleRatio =
+        item.saleRatio !== 0
+          ? item.saleRatio * 100 + '%'
+          : '';
+      let currentPrice =
+        item.price
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '원';
+      let salePrice =
+        item.salePrice !== 0
+          ? item.salePrice
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '원'
+          : '';
+      let img = item.image.thumbnail;
+      let alt = item.image.alt;
+      let description = item.description;
+
+      insertLast(
+        getItemList,
+        /* html */
+        `<a
+        href="./productDetail.html"
+        class="product__item"
+      >
+        <div class="product__list__img1">
+          <div class="product__list__img2">
+            <img
+              src="${item.image.thumbnail}"
+              alt="${item.image.alt}"
+            />
+            <div>
+              <button
+                type="button"
+                class="cart-icon"
+              >
+                <img
+                  src="../css/img/Icon/Cart.svg"
+                  alt="카트아이콘"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="product__list__txt">
+          <span class="badge1">샛별배송</span>
+          <span class="badge2"
+            >${name}</span
+          >
+          <div class="badge3">
+            <div>
+              <span class="discount">${saleRatio}</span>
+              <span class="price">${salePrice}</span>
+            </div>
+            <div>
+              <span class="out_price">${currentPrice}</span>
+            </div>
+          </div>
+          <p class="badge4">
+            튀기지않아 부담없는 매콤함
+          </p>
+          <div class="badge5">
+            <span class="karly_only"
+              >karly only</span
+            >
+            <span class="limited_quantity"
+              >한정 수량</span
+            >
+          </div>
+        </div>
+      </a>`
+      );
+    });
+  });
